975ad2ab04415bd8b054a9595a4c0e891e900402
gustavo-donato-cardoso <gustavodonatocardoso13526@gmail.com>
2025-08-06 16:28:27 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main( ) {
+    
+    int x;
+    float y;
+    float s;
+    
+    x = 10;
+    y = 5.5;
+    s = x+y;
+    
+    printf("A soma dos valores d x e y eh : %f ", s);
+    
+}
