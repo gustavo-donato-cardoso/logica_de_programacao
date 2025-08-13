2eab8a6f88abcd666b27f583da6c330ae91b67d9
gustavo-donato-cardoso <gustavodonatocardoso13526@gmail.com>
2025-08-13 14:04:12 -0300

    Add files via upload

---
+++ corrida.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+  int main()
+  {
+  
+    int evento = 5;
+    char corrida = 'c';
+    float tempo = 27.25;
+    
+    printf("O melhor tempo da eliminiatoria %c", corrida);
+    printf("\n do evento %d foi %f", evento, tempo);
+    }
